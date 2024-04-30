52e6b5ac54ac3a1afbd32edc6c3a83a0c76dd408
RoyadaSaleh <royada.saleh@gmail.com>
2024-04-29 22:13:15 -0400

    created edit page

--- Project4/src/main.jsx
+++ Project4/src/main.jsx
@@ -8,6 +8,11 @@ import "./index.css";
 import Root from "./routes/root";
 import ErrorPage from "./error-page";
 import Contact from "./routes/contact";
+import Root, { 
+  loader as rootLoader,
+  action as rootAction,
+ } from "./routes/root";
+
 
 
 const router = createBrowserRouter([
@@ -15,6 +20,8 @@ const router = createBrowserRouter([
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
     children: [
       {
         path: "contacts/:contactId",
