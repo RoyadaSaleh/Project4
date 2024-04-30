403c704231f8d065b930f71e9aa18081ef05b3df
RoyadaSaleh <royada.saleh@gmail.com>
2024-04-29 21:03:34 -0400

    changed and added links to error page

--- Project4/src/main.jsx
+++ Project4/src/main.jsx
@@ -5,11 +5,22 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import "./index.css";
+import Root from "./routes/root";
+import ErrorPage from "./error-page";
+import Contact from "./routes/contact";
+
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
   },
 ]);
 
